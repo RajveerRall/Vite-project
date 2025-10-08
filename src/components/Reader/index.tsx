0eaf2c0836b8701c6940572ed8a418c5d0f9da3a
Rajveer <rallrajveer@gmail.com>
2025-10-08 21:42:14 +0530

    book removed new packaged added

--- src/components/Reader/index.tsx
+++ src/components/Reader/index.tsx
@@ -313,12 +313,12 @@ const Reader: React.FC = () => {
       <div className="reader-main">
         <div
           className="epub-content"
-          onClick={isMobile ? undefined : handlePageClick}
+          onClick={handlePageClick}
           style={{ whiteSpace: 'pre-wrap' }}
           dangerouslySetInnerHTML={{ __html: ttsHighlightedContent || currentContent }}
         />
 
-        {!isMobile && showNavigationArrows && (
+        {showNavigationArrows && (
           <>
             <button
               onClick={() => handleChapterNavigation('prev')}
